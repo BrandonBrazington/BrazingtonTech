cc8c4082ce052611c022d4b2f96ea44844e41060
Brandon Brazington <brandon@brazington.tech>
2019-10-08 16:23:52 -0700

    Fix when update message is received more than once

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,13 +1,15 @@
+let lastMessageTimes = {
+  Balcony: -1,
+  Bedroom: -1,
+  LivingRoom: -1
+};
+
+let millisecondsRefreshTimeAfterWebsiteUpdate = -1;
+
 $(document).ready(function () {
   // Redirect to HTTPS if not already
   if (document.location.protocol != "https:" & document.location.host == "www.brazington.tech") {
     window.location.replace('https://www.brazington.tech/');
-  }
-
-  let lastMessageTimes = {
-    Balcony: -1,
-    Bedroom: -1,
-    LivingRoom: -1
   }
 
   // SignalR Stuff below here
@@ -17,58 +19,9 @@ $(document).ready(function () {
     .configureLogging(signalR.LogLevel.Information)
     .build();
 
-  connection.on("newMessage", (message) => {
-    console.log('Received message: ' + JSON.stringify(message));
-    // Check if has deviceID and don't continue if it doesn't
-    if (!message.deviceID) {
-      console.log("Invalid message received (no deviceID)");
-      return;
-    }
+  connection.on("newMessage", handleNewMessage);
 
-    // Process Balcony Temperature Data
-    if (message.deviceID == "Balcony") {
-      let currentDateTime = new Date();
-      lastMessageTimes.Balcony = currentDateTime.getTime();
-      let currentDateTimeString = (currentDateTime.getMonth() + 1).toString() + "/" + currentDateTime.getDate().toString() + " " + currentDateTime.getHours().toString() + ":" + currentDateTime.getMinutes().toString().padStart(2, '0') + ":" + currentDateTime.getSeconds().toString().padStart(2, '0');
-      $('#outdoor-temperature-data').html(message.temperature + "&deg;");
-      updateLastRefreshedCounters();
-    }
-
-    // Process Bedroom Temperature Data
-    if (message.deviceID == "Bedroom") {
-      let currentDateTime = new Date();
-      lastMessageTimes.Bedroom = currentDateTime.getTime();
-      let currentDateTimeString = (currentDateTime.getMonth() + 1).toString() + "/" + currentDateTime.getDate().toString() + " " + currentDateTime.getHours().toString() + ":" + currentDateTime.getMinutes().toString().padStart(2, '0') + ":" + currentDateTime.getSeconds().toString().padStart(2, '0');
-      $('#bedroom-temperature-data').html(message.temperature + "&deg;");
-      $('#bedroom-humidity-data').html(message.humidity + "%");
-      updateLastRefreshedCounters();
-    }
-
-    // Process Living Room Temperature Data
-    if (message.deviceID == "LivingRoom") {
-      let messageDateTime = new Date(message.time * 1000);
-      lastMessageTimes.LivingRoom = message.time * 1000;
-      let messageDateTimeString = (messageDateTime.getMonth() + 1).toString() + "/" + messageDateTime.getDate().toString() + " " + messageDateTime.getHours().toString() + ":" + messageDateTime.getMinutes().toString().padStart(2, '0') + ":" + messageDateTime.getSeconds().toString().padStart(2, '0');
-      $('#living-room-temperature-data').html(message.temperature + "&deg;");
-      $('#living-room-humidity-data').html(message.humidity + "%");
-      updateLastRefreshedCounters();
-    }
-  });
-
-  connection.on("updated", (secondsToWait) => {
-    console.log(`Website has been updated, reloading page in ${secondsToWait} seconds`);
-    $("#updated-banner").removeAttr("hidden");
-    let pageRefreshTimer = $("#page-refresh-timer");
-    setInterval(() => {
-      secondsToWait -= 1;
-      if (secondsToWait <= 0) {
-        location.reload(true);
-      }
-      let minutes = parseInt(secondsToWait / 60, 10) % 60
-      let seconds = (secondsToWait % 60).toString().padStart(2, "0")
-      pageRefreshTimer.text(minutes + ":" + seconds)
-    }, 1000);
-  });
+  connection.on("updated", handleUpdatedMessage);
 
   async function start() {
     try {
@@ -91,29 +44,6 @@ $(document).ready(function () {
   });
 
   start();
-
-  function calculateSecondsBetweenMilliseconds(start, end) {
-    return Math.round((end - start) / 1000);
-  }
-
-  function updateLastRefreshedCounters() {
-    let currentDateTime = new Date();
-    let currentMilliseconds = currentDateTime.getTime();
-    if (lastMessageTimes.Bedroom != -1) {
-      let bedroomSecondsSinceLastUpdate = calculateSecondsBetweenMilliseconds(lastMessageTimes.Bedroom, currentMilliseconds);
-      $("#bedroom-humidity-time").text(bedroomSecondsSinceLastUpdate + " seconds ago")
-      $("#bedroom-temperature-time").text(bedroomSecondsSinceLastUpdate + " seconds ago")
-    }
-    if (lastMessageTimes.LivingRoom != -1) {
-      let livingRoomSecondsSinceLastUpdate = calculateSecondsBetweenMilliseconds(lastMessageTimes.LivingRoom, currentMilliseconds);
-      $("#living-room-humidity-time").text(livingRoomSecondsSinceLastUpdate + " seconds ago")
-      $("#living-room-temperature-time").text(livingRoomSecondsSinceLastUpdate + " seconds ago")
-    }
-    if (lastMessageTimes.Balcony != -1) {
-      let balconySecondsSinceLastUpdate = calculateSecondsBetweenMilliseconds(lastMessageTimes.Balcony, currentMilliseconds);
-      $("#outdoor-temperature-time").text(balconySecondsSinceLastUpdate + " seconds ago")
-    }
-  }
 
   setInterval(() => {
     let currentDateTime = new Date();
@@ -146,3 +76,83 @@ $(document).ready(function () {
   // $('#living-room-temperature-time').text(currentDateTimeString);
   // $('#living-room-humidity-time').text(currentDateTimeString);
 });
+
+function handleUpdatedMessage(secondsToWait) {
+  console.log(`Website has been updated, reloading page in ${secondsToWait} seconds`);
+  let currentDateTime = new Date();
+  let currentMilliseconds = currentDateTime.getTime();
+  millisecondsRefreshTimeAfterWebsiteUpdate = currentMilliseconds + (secondsToWait * 1000);
+  updateLastRefreshedCounters();
+  $("#updated-banner").removeAttr("hidden");
+}
+
+function handleNewMessage(message) {
+  console.log('Received message: ' + JSON.stringify(message));
+  // Check if has deviceID and don't continue if it doesn't
+  if (!message.deviceID) {
+    console.log("Invalid message received (no deviceID)");
+    return;
+  }
+
+  // Process Balcony Temperature Data
+  if (message.deviceID == "Balcony") {
+    let currentDateTime = new Date();
+    lastMessageTimes.Balcony = currentDateTime.getTime();
+    let currentDateTimeString = (currentDateTime.getMonth() + 1).toString() + "/" + currentDateTime.getDate().toString() + " " + currentDateTime.getHours().toString() + ":" + currentDateTime.getMinutes().toString().padStart(2, '0') + ":" + currentDateTime.getSeconds().toString().padStart(2, '0');
+    $('#outdoor-temperature-data').html(message.temperature + "&deg;");
+    updateLastRefreshedCounters();
+  }
+
+  // Process Bedroom Temperature Data
+  if (message.deviceID == "Bedroom") {
+    let currentDateTime = new Date();
+    lastMessageTimes.Bedroom = currentDateTime.getTime();
+    let currentDateTimeString = (currentDateTime.getMonth() + 1).toString() + "/" + currentDateTime.getDate().toString() + " " + currentDateTime.getHours().toString() + ":" + currentDateTime.getMinutes().toString().padStart(2, '0') + ":" + currentDateTime.getSeconds().toString().padStart(2, '0');
+    $('#bedroom-temperature-data').html(message.temperature + "&deg;");
+    $('#bedroom-humidity-data').html(message.humidity + "%");
+    updateLastRefreshedCounters();
+  }
+
+  // Process Living Room Temperature Data
+  if (message.deviceID == "LivingRoom") {
+    let messageDateTime = new Date(message.time * 1000);
+    lastMessageTimes.LivingRoom = message.time * 1000;
+    let messageDateTimeString = (messageDateTime.getMonth() + 1).toString() + "/" + messageDateTime.getDate().toString() + " " + messageDateTime.getHours().toString() + ":" + messageDateTime.getMinutes().toString().padStart(2, '0') + ":" + messageDateTime.getSeconds().toString().padStart(2, '0');
+    $('#living-room-temperature-data').html(message.temperature + "&deg;");
+    $('#living-room-humidity-data').html(message.humidity + "%");
+    updateLastRefreshedCounters();
+  }
+}
+
+function calculateSecondsBetweenMilliseconds(start, end) {
+  return Math.round((end - start) / 1000);
+}
+
+function updateLastRefreshedCounters() {
+  let currentDateTime = new Date();
+  let currentMilliseconds = currentDateTime.getTime();
+  if (lastMessageTimes.Bedroom != -1) {
+    let bedroomSecondsSinceLastUpdate = calculateSecondsBetweenMilliseconds(lastMessageTimes.Bedroom, currentMilliseconds);
+    $("#bedroom-humidity-time").text(bedroomSecondsSinceLastUpdate + " seconds ago")
+    $("#bedroom-temperature-time").text(bedroomSecondsSinceLastUpdate + " seconds ago")
+  }
+  if (lastMessageTimes.LivingRoom != -1) {
+    let livingRoomSecondsSinceLastUpdate = calculateSecondsBetweenMilliseconds(lastMessageTimes.LivingRoom, currentMilliseconds);
+    $("#living-room-humidity-time").text(livingRoomSecondsSinceLastUpdate + " seconds ago")
+    $("#living-room-temperature-time").text(livingRoomSecondsSinceLastUpdate + " seconds ago")
+  }
+  if (lastMessageTimes.Balcony != -1) {
+    let balconySecondsSinceLastUpdate = calculateSecondsBetweenMilliseconds(lastMessageTimes.Balcony, currentMilliseconds);
+    $("#outdoor-temperature-time").text(balconySecondsSinceLastUpdate + " seconds ago")
+  }
+  if (millisecondsRefreshTimeAfterWebsiteUpdate != -1) {
+    secondsLeftBeforeRefresh = calculateSecondsBetweenMilliseconds(currentMilliseconds, millisecondsRefreshTimeAfterWebsiteUpdate);
+    if (secondsLeftBeforeRefresh <= 0) {
+      caches.delete("pwabuilder-offline");
+      location.reload(true);
+    }
+    let minutes = parseInt(secondsLeftBeforeRefresh / 60, 10) % 60
+    let seconds = (secondsLeftBeforeRefresh % 60).toString().padStart(2, "0")
+    $("#page-refresh-timer").text(minutes + ":" + seconds)
+  }
+}
